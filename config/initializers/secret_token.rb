b7fd5877f8638c4f35db0e02ae5ccd1e540c9d15
jayganta <jayganta@me.com>
2012-11-05 23:47:24 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newapper::Application.config.secret_token = 'e49f334dc607c88f7a2ed0d1d949651e3712128860064bfe8b197de2ea452c72263d49f5321676ef6500d2e70a34b0178b42fa81d73587e1ce2ae707182daa96'
